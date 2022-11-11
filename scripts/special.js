f212868a327c512f6e7f70d1da8c741f07f4dd03
anup-kgp <dasanup1999.com>
2022-11-11 16:08:54 +0530

    adding footer

--- scripts/special.js
+++ scripts/special.js
@@ -1,3 +1,16 @@
+import navbar from "../components/nav.js"
+import {footer} from "../components/navbar.js"
+import {lastbar} from "../components/navbar.js"
+let last=document.getElementById("last_bar")
+last.innerHTML=lastbar()
+let foot=document.getElementById("footer")
+foot.innerHTML=footer()
+
+
+let nav=document.getElementById("navbar");
+nav.innerHTML=navbar();
+console.log(nav);
+
 let data = [
     
     {
