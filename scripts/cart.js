978e21124dd7e12785b67d374eb89d3215b331a0
Gayathri Harikumar <gayathri.hk175@gmail.com>
2022-11-11 11:52:11 +0530

    more cart

--- scripts/cart.js
+++ scripts/cart.js
@@ -2,19 +2,30 @@ let cart = JSON.parse(localStorage.getItem("add1"));
 
 let proceedForPay = document.querySelector("#payment");
 proceedForPay.addEventListener("click",() =>{
-    window.location.href="./payment.html";
+    window.location.href="../PaymentPage/payment.html";
 })
 
-let itemNum = cart.length;
-let lengthDisplay = document.querySelector(".cart___lengthText");
-lengthDisplay.innerText = `Cart : ${itemNum}`;
+let promoCodeBtn = document.querySelector("#promo___Code");
+
+let renderLength = () => {
+    let itemNum = cart.length;
+    let lengthDisplay = document.querySelector(".cart___lengthText");
+    lengthDisplay.innerText = `Cart : ${itemNum}`;
+}
+
+renderLength();
 
 
 let container = document.querySelector("section");
 
+
 let displayProducts =(cart) =>{
     container.innerHTML = null;
-    cart.forEach(({image,name,description,price},index)=>{
+    cart.forEach((ele,index)=>{
+
+        // console.log(ele);
+
+        const {qty,image,name,description,price} = ele;
 
         let productDiv = document.createElement("div");
         productDiv.setAttribute("id","prodBox");
@@ -35,6 +46,25 @@ let displayProducts =(cart) =>{
         productAmount.setAttribute("id","prodAmt");
         productAmount.innerText = `$` + price;
 
+        // 
+
+        let quantityP = document.createElement("p");
+        quantityP.setAttribute("class","quantity_Number");
+
+        let incDecDiv = document.createElement("div");
+        incDecDiv.setAttribute("class","inc__Dec__Div");
+
+        let incrementQty = document.createElement("button");
+        incrementQty.setAttribute("class","plus");
+        let decrementQty = document.createElement("button");
+        decrementQty.setAttribute("class","minus");
+
+        quantityP.innerText = `Quantity - ${qty}`;;
+        incrementQty.innerText = "+";
+        decrementQty.innerText= "-";
+
+        // 
+
         let productButton = document.createElement("button");
         productButton.setAttribute("id","prodBtn");
         productButton.innerText = "Remove";
@@ -42,7 +72,19 @@ let displayProducts =(cart) =>{
             deleteFromCart(index);
         }
 
-        productDiv.append(productVisual,productName,productAmount,productButton);
+        //
+
+        incrementQty.addEventListener("click",()=>{
+            handleQuantity(ele , `+`);
+        })
+        decrementQty.addEventListener("click",()=>{
+            handleQuantity(ele, `-`);
+        })
+
+        //
+
+        incDecDiv.append(incrementQty,decrementQty);
+        productDiv.append(productVisual,productName,productAmount,quantityP,incDecDiv,productButton);
         container.append(productDiv);
     })
 }
@@ -50,16 +92,28 @@ let displayProducts =(cart) =>{
 displayProducts(cart);
 
 let totalPrice = () => {
+
     let sum = 0;
     cart.map((ele)=>{
-        sum += ele.price;
-
+        sum += ele.price *ele.qty;
         // console.log(ele);
+    })
+    promoCodeBtn.addEventListener("click", ()=>{
+        sum -= sum * 0.1;
+        console.log(sum);
+        let displayPrice = document.querySelector("#priceDisplay");
+        displayPrice.innerText = `Total Price : $${Math.round(sum)}`;
+        localStorage.setItem("total",JSON.stringify(Math.round(sum)));
+
+        promoCodeBtn.setAttribute("disabled","disabled");
+        promoCodeBtn.innerText = "Coupon Applied";
+        promoCodeBtn.style.backgroundColor = "gray";
+        promoCodeBtn.style.color = "white";
+        return;
     })
     let displayPrice = document.querySelector("#priceDisplay");
     displayPrice.innerText = `Total Price : $${Math.round(sum)}`;
     localStorage.setItem("total",JSON.stringify(Math.round(sum)));
-
 
 }
 
@@ -74,3 +128,27 @@ let deleteFromCart = (index) => {
     displayProducts(cart);
     totalPrice();
 }
+
+const handleQuantity = (data,type) =>{
+    if(type === "+"){
+        if(data.qty === 5){
+            alert("Can't add more than 5 products!");
+            return;
+        }
+        data.qty = data.qty + 1;
+        displayProducts(cart);
+        localStorage.setItem("add1",JSON.stringify(cart));
+        totalPrice()
+    }else{
+        if(data.qty === 1){
+            alert("Quantity can't be less than one");
+            return
+        }
+        data.qty = data.qty-1;
+        displayProducts(cart);
+        localStorage.setItem("add1",JSON.stringify(cart));
+        totalPrice()
+    }    
+    
+}
+
