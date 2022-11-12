5c9dc0e43f5b00f2c4dfee8240dc475488ad77f3
anup-kgp <dasanup1999.com>
2022-11-13 00:52:21 +0530

    lets go

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,10 @@
 import {navbar} from "../components/navbar.js";
+import {footer} from "../components/footer.js"
+import {lower} from "../components/lower.js"
+let low=document.getElementById("lower");
+low.innerHTML=lower();
+let foot=document.getElementById("footer");
+foot.innerHTML=footer();
 let navBox = document.querySelector("#navbar____Container")
 navBox.innerHTML = navbar();
 
