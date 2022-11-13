06b3277f7bf8b88ff7da8d31245a484ec0f645c3
anup-kgp <dasanup1999.com>
2022-11-13 13:26:07 +0530

    minor_change_on_admin page

--- Admin page/login.js
+++ Admin page/login.js
@@ -3,7 +3,7 @@
 let login=()=>{
     let email_id=document.getElementById("mail").value;
     let password=document.getElementById("pswd").value;
-    if(email_id=="rodan&fields@gmail.com"&&password=="fieldsofrodan"){
+    if(email_id=="admin@gmail.com"&&password=="admin"){
         alert("Login Successful");
         window.location.href="admin.html";
     }
